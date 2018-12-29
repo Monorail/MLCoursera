2a68fc89c762b96eed07a2e2735ba4c35a677f5a
Alex Berliner <aberliner@mllwl597-l.magicleap.ds>
2018-12-29 12:46:50 -0500

    plotData

--- Assignment_1/machine-learning-ex1/ex1/plotData.m
+++ Assignment_1/machine-learning-ex1/ex1/plotData.m
@@ -16,8 +16,10 @@ figure; % open a new figure window
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-
-
+figure(1)
+plot(x,y, 'rx', 'MarkerSize', 10);
+xlabel('population size in 10,000s');
+ylabel('profit in $10,000s');
 
 
 % ============================================================
