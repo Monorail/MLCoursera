6cb3cea92a3c706faa4e576fef80db333e11ba2b
Alex Berliner <aberliner@mllwl597-l.magicleap.ds>
2018-12-29 14:51:42 -0500

    gradientDescent

--- Assignment_1/machine-learning-ex1/ex1/gradientDescent.m
+++ Assignment_1/machine-learning-ex1/ex1/gradientDescent.m
@@ -1,32 +1,39 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
-%   taking num_iters gradient steps with learning rate alpha
+    %GRADIENTDESCENT Performs gradient descent to learn theta
+    %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
+    %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
+    % Initialize some useful values
+    m = length(y); % number of training examples
+    J_history = zeros(num_iters, 1);
+    % num_iters = 5
+    % theta
+    for iter_i = 1:num_iters
 
-for iter = 1:num_iters
+        % ====================== YOUR CODE HERE ======================
+        % Instructions: Perform a single gradient step on the parameter vector
+        %               theta.
+        %
+        % Hint: While debugging, it can be useful to print out the values
+        %       of the cost function (computeCost) and gradient here.
+        %
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta.
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-    X
+        % J_d = computeCost(X, y, theta)
+        cost = zeros(length(theta),1);
+        xty = (X * theta) - y; % 97 element column vector
+        for iter_j = 1:length(theta)
+            cost(iter_j) = (1/m) * xty' * X(:,iter_j);
+        end
+        % cost
+        theta = theta - alpha * cost;
 
 
 
-    % ============================================================
+        % ============================================================
 
-    % Save the cost J in every iteration
-    J_history(iter) = computeCost(X, y, theta);
+        % Save the cost J in every iteration
+        J_history(iter_i) = computeCost(X, y, theta);
 
-end
+    end
 
 end
